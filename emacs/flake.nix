edd19e80ec549d6f8183d44d4c26b345ef61e9de
Hoid <github@oedon.me>
2023-05-23 11:09:35 +0200

    feat: emacs-unstable

---
+++ emacs/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Neovim Nightly";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs = inputs @ { self, nixpkgs-unstable, ... }:
+    let
+      system = "x86_64-linux";
+
+      # Neovim nightly overlay
+      emacs = inputs.emacs-overlay.overlay;
+
+      # All the pkgs options
+      pkgs = import nixpkgs-unstable {
+        allowUnfree = true;
+        overlays = [ emacs ];
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          emacsUnstable # from the overlay. Check master flake for overlayAttrs
+        ];
+      };
+    };
+}
