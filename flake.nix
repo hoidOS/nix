0bb0d14fc8d84b64c4eeb8e9b2df6c5f1662afab
Hoid <github@oedon.me>
2025-07-16 07:43:28 +0200

    feat: ruby

--- flake.nix
+++ flake.nix
@@ -1,32 +1,38 @@
 {
   description = "Hoid's flake templates";
 
-  outputs = { self, ... }: {
-    templates = {
-      c = {
-        path = ./c;
-        description = "C Env";
-      };
-      emacs = {
-        path = ./emacs;
-        description = "Emacs Unstable";
-      };
-      install = {
-        path = ./install;
-        description = "Install Flake";
-      };
-      neovim = {
-        path = ./neovim;
-        description = "Neovim Nightly";
-      };
-      ref = {
-        path = ./ref;
-        description = "Reference Template";
-      };
-      rust = {
-        path = ./rust;
-        description = "Rust Shell";
+  outputs =
+    { self, ... }:
+    {
+      templates = {
+        c = {
+          path = ./c;
+          description = "C Env";
+        };
+        emacs = {
+          path = ./emacs;
+          description = "Emacs Unstable";
+        };
+        install = {
+          path = ./install;
+          description = "Install Flake";
+        };
+        neovim = {
+          path = ./neovim;
+          description = "Neovim Nightly";
+        };
+        ref = {
+          path = ./ref;
+          description = "Reference Template";
+        };
+        rust = {
+          path = ./rust;
+          description = "Rust Shell";
+        };
+        ruby = {
+          path = ./ruby;
+          description = "Ruby Shell";
+        };
       };
     };
-  };
 }
