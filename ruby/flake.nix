6811187d5dce91c11209361b625da9267eb4b480
Hoid <github@oedon.me>
2025-07-16 07:42:22 +0200

    feat: ruby

---
+++ ruby/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Ruby";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          pkg-config
+          libyaml.dev
+        ];
+      };
+    };
+}
